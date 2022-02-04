227d86ec057fcb8b9997cb387faba65ed542914c
Anh Le <kunquan2345@gmail.com>
2022-02-04 05:49:08 -0800

    Add JUnit and the test fail first

---
+++ SkillDemo.java
@@ -0,0 +1,5 @@
+class SkillDemo{
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+
+    @Test
+    public void addNumbersTest() {
+        assertEquals(3, SkillDemo.addNumbers(1, 1));
+    }
+}
